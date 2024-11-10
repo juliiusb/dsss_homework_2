0040b1819e81cf81d859125eb02a637bf35942e6
iamJuliusB <juliusbecker9@gmail.com>
2024-11-10 12:12:00 +0100

    added files for pip

---
+++ math_quiz/__init__.py
@@ -0,0 +1,3 @@
+from .math_quiz import math_quiz, random_int, random_operator, calculate
+
+__all__ = ["math_quiz", "random_int", "random_operator", "calculate"]
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mathquiz",                    # Package name
+    version="1.0.0",                    # Version number
+    packages=find_packages(),           # Finds the mathquiz package
+    install_requires=[],                # Add dependencies if needed
+    description="A math quiz for the DSSS homework.",
+    author="Julius Becker",                 # Replace with your name
+)
